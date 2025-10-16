4c34136d9ba3418eabe34e0c9626e602e24be278
Ayesh-J <jamadarayesh9@gmail.com>
2025-10-16 14:16:34 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-6xl text-red-600 bg-yellow-200 p-4">
+        Site Under Construction
+      </h1>
+    </>
+  )
+}
+
+export default App
